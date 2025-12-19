1d3df143856b6f3aeb15e8b9bef73b564e9af8fb
Adam Hymus <hymusprint@live.com>
2025-12-19 00:06:36 +0000

    Add PWA manifest and icon metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,11 +18,31 @@ const tektur = Tektur({
 export const metadata = {
   title: "Login",
   description: "Login to continue",
+  manifest: "/manifest.json",
+  themeColor: "#d0f500",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "black-translucent",
+    title: "Push Progress",
+  },
+  icons: {
+    icon: "/images/icon/icon-192.png",
+    apple: "/images/icon/icon-192.png",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="dark">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/images/icon/icon-192.png" />
+        <meta name="theme-color" content="#d0f500" />
+        <link
+          href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
+          rel="stylesheet"
+        />
+      </head>
       <body className={`${roboto.variable} ${tektur.variable} antialiased`}>
         <Providers>{children}</Providers>
       </body>
